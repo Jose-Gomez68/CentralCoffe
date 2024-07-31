bd8f7c15fdd2d30998bd4093af0bbbfaedfc7be5
joseGomez <jose_armando_za@hotmail.com>
2024-07-30 21:23:04 -0600

    add to LoginActivity.kt

---
+++ app/src/main/java/com/example/salestapapp/login/view/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.salestapapp.login.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.salestapapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
